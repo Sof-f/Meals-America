d3d14c574547dec28e434a91286f403568764d23
Sof-f <sofiakrasnicenko@gmail.com>
2026-07-17 15:56:06 +0300

    info page part 1

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Главная страница (localhost:5000/)
 @app.route('/')
 def index():
     return render_template('main.html')
 
-# Ваш старый маршрут, который вы прописали в HTML
 @app.route('/about')
-def page1():
+def about_page1():
     return render_template('about.html')
 
+@app.route('/how')
+def how_page():
+    return render_template('how.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
